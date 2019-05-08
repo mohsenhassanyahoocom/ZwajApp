e1b56fd347799acd87d2618c53cb3fcca6b97f1e
mohsenhassanyahoocom <mohsen_hassan@yahoo.com>
2019-05-08 18:04:17 +0200

    Initial Commit

---
+++ ZwajApp.API/Controllers/ValuesController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ZwajApp.API.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace ZwajApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public ValuesController(DataContext context)
+        {
+            _context = context;
+
+        }
+        // Get http://localhost:5000/api/values
+        // GET api/values
+        [HttpGet]
+        // public ActionResult<IEnumerable<string>> Get()
+        public async Task<IActionResult> GetValues()
+        {
+            // throw new Exception("Test Exception");
+            // return new string[] { "value1", "value2" };
+            // return new string[] { "Amir", "Muhammad", "Ali" };
+            var values = await _context.Values.ToListAsync();
+            return Ok(values);
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        // public ActionResult<string> Get(int id)
+        public async Task<IActionResult> GetValue(int id)
+        {
+            // return "value";
+            // return $"value {id}";
+            var value = await _context.Values.FirstOrDefaultAsync(x => x.id == id);
+            return Ok(value);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
